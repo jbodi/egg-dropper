3af9b65109a135660c8a5a5d2765d982c86fead5
juanl <juanl@DESKTOP-5USFDEO>
2018-08-02 18:00:34 +0200

    Second try!
    Almost done! Matrix done to improve the efficiency of the program but
    still has errors.

--- src/eggDropper/EggDropper.java
+++ src/eggDropper/EggDropper.java
@@ -1,72 +1,100 @@
 package eggDropper;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class EggDropper {
 
 	public static void main(String[] args) {
-		int floors 	= 100;
-		int eggs 	= 100;
+		
+		System.out.println("In a 100-floor skyscraper with 100 eggs, the minimun number of attempts of dropping an egg "
+							+ "to find out the highest floor that doesn't break the eggs is: " + minEggDropper100());
+		
+		System.out.println("In a 100-floor skyscraper with 2 eggs, the minimun number of attempts of dropping an egg "
+				+ "to find out the highest floor that doesn't break the eggs is: " + minEggDropper2eggs100Floors());
+		
+		
+		int floors 	= 2;
+		int eggs	= 0;
 		
 		System.out.println("In a " + floors + "-floor skyscraper with " + eggs + " eggs, the minimun number of attempts of dropping an egg "
-							+ "to find out the highest floor that doesn't break the eggs is: " + minEggDropper100(floors, eggs));
+				+ "to find out the highest floor that doesn't break the eggs is: " + minEggDropperCalc(floors, eggs));
 	}
 
-	public static int minEggDropper100(int totalFloors, int eggs) {
-		
-		List<Integer> floors;
-		
-		//
-		floors = new ArrayList<Integer>();
-		for(int i = 1; i <= totalFloors; i++) {
-			floors.add(i);
-		}
-		
-		System.out.println(floors.size());
-		
-		
-		return minEggDropperRec(floors, eggs);
+	public static int minEggDropper100() {
+				
+		return minEggDropperCalc(100, 100);
 	}
 	
-	public static int minEggDropperRec(List<Integer> floors, int eggs) {
-		int tryFloor 	= (floors.size() / 2);
-		int min 		= Integer.MAX_VALUE;
-		int res			= 0;
+	public static int minEggDropper2eggs100Floors() {
 		
+		return minEggDropperCalc(100, 2);
+	}
+	
+	public static int minEggDropperCalc(int floors, int eggs) {
 		
+		//The minimum number of egg drops we need to find the criticalFloor, the highest floor we can throw
+		//an egg without breaking the egg its minDrops100. To calculate this number we need to calculate
+		//all the posible combination that can happen and save for each floor the minimum number of throws.
+		//We calculate this dropping the eggs from every floor.
+
 		
-		while(floors.size() != 0) {
-			
-			if(floors.size() == 1) {
-				return 1;
-			}
-			
-			if(eggs == 1) {
-				return floors.size();
-			}
-			
-			List<Integer> floorsAbove = new ArrayList<Integer>(); 
-			floorsAbove.addAll(floors.subList(tryFloor, floors.size()));
-			
-			List<Integer> floorsBelow = new ArrayList<Integer>(); 
-			floorsBelow.addAll(floors.subList(0, tryFloor));
-			
-			floors.removeAll(floorsAbove);
-			floors.removeAll(floorsBelow);
-			
-			res = 1 + Math.max(minEggDropperRec(floorsAbove, eggs), minEggDropperRec(floorsBelow, eggs - 1));
-			
-//			System.out.println("huevos = " + eggs + "|| pisos = " + floors.size() );
-//			System.out.println("res = " + res);
-			
-//			if(res < min) {
-//				min = res;
-//			}
-			
+		//We are going to use a matrix to save all the different combinations that could happen.
+		//This matrix is a matrix of (floors + 1) X (eggs + 1). We add 1 because we need the combination
+		//with 0 eggs and 0 floors too.
+		int[][] eggDropperMatrix = new int[floors + 1][eggs + 1];
+		
+		//Firstly, for 0 floors and 0 eggs its will always be a 0:
+		eggDropperMatrix[0][0] = 0;
+		
+		//Then, with 0 eggs you can't throw any eggs. Same with 0 floors:
+		for(int i = 1; i <= floors; i++) {
+			eggDropperMatrix[i][0] = 0;
 		}
 		
-		return res;
+		for(int i = 1; i <= eggs; i++) {
+			eggDropperMatrix[0][i] = 0;
+		}
+		
+		//With only 1 egg, we need to do the same throws as floors
+		for(int i = 1; i <= floors; i++) {
+			eggDropperMatrix[i][1] = i;
+		}
+		
+		//And with 1 floor we only need 1 throw.
+		for(int i = 1; i <= eggs; i++) {
+			eggDropperMatrix[1][i] = 1;
+		}
+		
+		//Then is necessary to fill all the other elements of the matrix.
+		//Iteration in the matrix. It starts at index 2 in both axis as we had already filled the elements before.
+		for(int x = 2; x <= floors; x++) {
+			
+			for(int y = 2; y <= eggs; y++) {
+				
+				//We need to save the minimum number of throws so a variable "minimum" its necessary for each
+				//element in the matrix. This value is saved in this same element. The default its the maximum
+				//possible integer so we can be sure it's going to change.
+				eggDropperMatrix[x][y] = Integer.MAX_VALUE;
+				
+				//As we need to try every combination we are going to "throw" an egg from every floor and then lookup
+				//for the maximum value from floor 1 to reach floor 'x'.
+				for(int i = 1; i <= x; i++) {
+					
+					//We need the maximum element for the next throw between the element if the eggs does'nt break
+					//(so we have the same number of eggs but we only try with the number of the upper floors)
+					//and when it does break (1 egg less and the same floors we are now minus 1). 
+					//We add 1 throw more to the result, this "throw".
+					int numberOfThrows = 1 + Math.max(eggDropperMatrix[x - i][y], eggDropperMatrix[i - 1][y - 1]);
+					
+					//As its said before, we need to save the minimum for each element.
+					if(numberOfThrows < eggDropperMatrix[x][y]) {
+						eggDropperMatrix[x][y] = numberOfThrows;
+					}
+					
+				}
+			}
+		}
+		
+		//Finally, we return the element we were looking for
+		return eggDropperMatrix[floors][eggs];
 	}
 	
 }
--- src/test/EggDropperTest.java
+++ src/test/EggDropperTest.java
@@ -2,9 +2,6 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.ArrayList;
-
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import eggDropper.EggDropper;
@@ -15,22 +12,16 @@ class EggDropperTest {
 
 	@Test
 	void testMinEggDropper1FloorXEgg() {
-		ArrayList<Integer> floor = new ArrayList<Integer>();
-		floor.add(1);
-		int minDrops = EggDropper.minEggDropperRec(floor, 1);
+
+		int minDrops = EggDropper.minEggDropperCalc(1, 20);
 		
 		assertEquals(1, minDrops);
 	}
 
 	@Test
 	void testMinEggDropper5Floors1Egg() {
-		ArrayList<Integer> floors = new ArrayList<Integer>();
-		floors.add(1);
-		floors.add(2);
-		floors.add(3);
-		floors.add(4);
-		floors.add(5);
-		int minDrops = EggDropper.minEggDropperRec(floors, 1);
+
+		int minDrops = EggDropper.minEggDropperCalc(5, 1);
 		
 		assertEquals(5, minDrops);
 	}
